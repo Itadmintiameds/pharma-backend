2409367c7eb5fd322c591f4de99e3f3178675034
ShreerakshaRao <your_github_shreeraksha@tiameds.ai>
2026-06-16 12:12:18 +0530

    Initial Commit

---
+++ src/main/java/tiameds/pharmabackend/PharmaBackendApplication.java
@@ -0,0 +1,13 @@
+package tiameds.pharmabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tiameds/pharmabackend/PharmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tiameds.pharmabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
